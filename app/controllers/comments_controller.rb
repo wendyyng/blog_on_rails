f626a33b41a05fccee5c76e288206411b5c65c88
Wendy Ng <yng@ucalgary.ca>
2022-07-17 23:59:56 +0000

    Add comments controller and actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+    # =============CALLBACKS=====================
+    before_action :find_post
+    # ==============CREATE========================
+    def create
+
+    end
+
+    # ================DELETE=========================
+    def destroy
+
+    end
+
+    private
+
+    def find_post
+
+    end
+end
