546ff9021ed4b1b263f77ae173c527cae3805897
Wendy Ng <yng@ucalgary.ca>
2022-07-18 00:22:28 +0000

    Add views for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+  
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
+
 end
