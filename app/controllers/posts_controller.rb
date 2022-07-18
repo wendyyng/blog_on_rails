ca3c23e48998180fd072c0ba001a9f7921bc2c8a
Wendy Ng <yng@ucalgary.ca>
2022-07-18 02:59:51 +0000

    Edited posts and comments controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,37 +8,48 @@ class PostsController < ApplicationController
     end
 
     def create
-
+        @post = Post.new(post_params)
+        if @post.save
+        flash[:notice]= "Post created successfully!"
+        redirect_to post_path(@post)
+        else
+        render :new
+        end
     end
     # ================READ========================
     def index
-
+        @posts = Post.order(created_at: :desc)
     end
 
     def show
-
+        @comments = @post.comments.order(created_at: :desc)
+        @comment = Comment.new
     end
     # ===============UPDATE==========================
     def edit
-
     end
 
     def update
-
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+          else
+            render :edit
+          end
     end
     # ================DELETE=========================
     def destroy
-
+        @post.destroy
+        redirect_to posts_path
     end
 
     private
 
     def find_post
-
+        @post = Post.find params[:id]
     end
 
     def post_params
-
+        params.require(:post).permit(:title, :body)
     end
 
 end
