c83cf837d2438f9f3a611abc0a232da1633dc18d
Wendy Ng <yng@ucalgary.ca>
2022-07-23 23:47:06 +0000

    Add cancan gem and authorize_user! to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
 
     # =============CALLBACKS=====================
     before_action :find_post, only: [:edit, :update, :show, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authorize_user!, only:[:edit, :update, :destroy]
     # ==============CREATE========================
     def new
         @post = Post.new
@@ -9,6 +11,7 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
+        @post.user = current_user
         if @post.save
         flash[:notice]= "Post created successfully!"
         redirect_to post_path(@post)
@@ -44,6 +47,10 @@ class PostsController < ApplicationController
     end
 
     private
+
+    def authorize_user!
+        redirect_to root_path, alert: "Not authorized" unless can?(:crud, @post)
+      end
 
     def find_post
         @post = Post.find params[:id]
