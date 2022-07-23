8e8cdba7265783edf8db491531815b4254553397
Wendy Ng <yng@ucalgary.ca>
2022-07-23 22:39:28 +0000

    Add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new params.require(:user).permit(
+          :name,
+          :email,
+          :password,
+          :password_confirmation
+        )
+        
+        if @user.save
+          session[:user_id] = @user.id
+          flash.notice = "Signed up!"
+          redirect_to root_path
+        else
+          render :new, status: 303
+        end 
+    
+      end
+end
