26652709d33cc1cfbdfcc3f5e025f7fccc1b9f76
Wendy Ng <yng@ucalgary.ca>
2022-07-23 23:42:03 +0000

    Add admin user and faker generated users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,28 @@
 
 Post.destroy_all
 Comment.destroy_all
+User.destroy_all
+
+# Admin User
+PASSWORD = "123"
+super_user = User.create(
+  name: "Admin",
+  email: "admin@user.com",
+  password: PASSWORD,
+  is_admin: true
+)
+
+10.times do 
+  name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    name: first_name,
+    email: "#{name.downcase}@#{last_name.downcase}.com",
+    password: PASSWORD
+  )
+end
+
+users = User.all
 
 50.times do
     created_at = Faker::Date.backward(days:365 * 5)
@@ -16,10 +38,11 @@ Comment.destroy_all
       body: Faker::Lorem.paragraph_by_chars(number: 55, supplemental: false),
       created_at: created_at,
       updated_at: created_at,
+      user: users.sample
     )
     if q.valid?
       rand(1..3).times do
-        Comment.create(body: Faker::Hacker.say_something_smart, post:q)
+        Comment.create(body: Faker::Hacker.say_something_smart, post:q, user: users.sample)
       end
     end
   end
@@ -29,5 +52,6 @@ Comment.destroy_all
 
 puts Cowsay.say("Generated #{posts.count} posts", :frogs)
 puts Cowsay.say("Generated #{comments.count} comments", :dragon)
+puts Cowsay.say("Generated #{users.count} users", :cow)
 
 # To run this file use command: rails db:seed
