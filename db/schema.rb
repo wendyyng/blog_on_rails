8f4c2f19c0dd7c4f706b89488251bfa939125cb8
Wendy Ng <yng@ucalgary.ca>
2022-07-23 23:07:09 +0000

    Migration user to posts and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_23_222844) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_230238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_23_222844) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_23_222844) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_23_222844) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
