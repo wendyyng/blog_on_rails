4007b2e424f88ec81df26e4c57d3cd461382e582
Wendy Ng <yng@ucalgary.ca>
2022-07-24 04:57:48 +0000

    Add find_use, user_params methods and update methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-
+   # =============CALLBACKS=====================
+   before_action :find_user, only: [:update]  
+  
+  # ==============CREATE========================
     def new
         @user = User.new
       end
@@ -21,13 +24,31 @@ class UsersController < ApplicationController
         end 
     
     end
-
+    # ===============UPDATE==========================
     def edit
       @user = current_user
     end
 
+    def update
+      if current_user.update(user_params)
+          redirect_to root_path
+        else
+          render :edit
+        end
+    end
+
     def pass_edit
       @user = current_user
+  end
+
+  private
+
+  def find_user
+    @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :users, only:[:new, :create, :edit]
+  resources :users, only:[:new, :create, :edit, :update]
   resource :session, only:[:new, :create, :destroy]
 
 
