3f8b0acd39797984f27914ec3c14fcf3fb80b044
Wendy Ng <yng@ucalgary.ca>
2022-07-25 01:47:58 +0000

    Adjust styling using bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 class UsersController < ApplicationController
   # =============CALLBACKS=====================
   before_action :find_user, only: [:update]  
-  before_action :authenticate_user!, only: [:update]
+  before_action :authenticate_user!, only: [:update, :edit]
 
  # ==============CREATE========================
  def new
